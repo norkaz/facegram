74bdd7e41efb32f5afb60f93f0ed1d05614000d7
Philip Ekblom <philip.ekblom@gmail.com>
2018-08-26 19:40:36 +0200

    Added Sass

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {NavMenu} from './components/navmenu/NavMenu';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
